804d230c5d2b21491098c80b5dc837637ad01d10
Tom Wells <tom.wells89@gmail.com>
2019-03-18 14:23:53 +0000

    added browser-snyc

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,37 @@
-var gulp = require('gulp');
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var cleanCss = require('gulp-clean-css')
+var sourcemaps = require('gulp-sourcemaps')
 
-gulp.task('default', function() {
-      // place code for your default task here
-    });
+var browserSync = require('browser-snyc').create();
+
+sass.compiler = require('node-sass')
+
+gulp.task('sass', function() {
+    return gulp.src("css/app.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(
+        cleanCss({
+            compatibility: 'ie8'
+        })
+    )
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("."))
+
+})
+
+gulp.task('watch', function () {
+
+    browserSync.init({
+        server: {
+            baseDir: '.'
+        }
+    })
+
+    gulp.watch("css/app.scss", ["sass"])
+})
+
+
+gulp.task('default', ["sass", "watch"])
+    // we wnat to run "sass css/app.scss app.css --watch"
